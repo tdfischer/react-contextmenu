3823a45d2a3ca580db322de5a57da2f1aa3ed5a2
Torrie Fischer <tdfischer@hackerbots.net>
2017-06-20 16:29:54 -0700

    build es5 too

---
+++ modules/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _ContextMenu = require('./ContextMenu');
+
+Object.defineProperty(exports, 'ContextMenu', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_ContextMenu).default;
+  }
+});
+
+var _ContextMenuTrigger = require('./ContextMenuTrigger');
+
+Object.defineProperty(exports, 'ContextMenuTrigger', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_ContextMenuTrigger).default;
+  }
+});
+
+var _MenuItem = require('./MenuItem');
+
+Object.defineProperty(exports, 'MenuItem', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_MenuItem).default;
+  }
+});
+
+var _SubMenu = require('./SubMenu');
+
+Object.defineProperty(exports, 'SubMenu', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_SubMenu).default;
+  }
+});
+
+var _connectMenu = require('./connectMenu');
+
+Object.defineProperty(exports, 'connectMenu', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_connectMenu).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
